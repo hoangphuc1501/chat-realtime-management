3eb74f66636fb37ead2f6660e39af12bac389118
hoangphuc1501 <katakurilancer.com>
2024-11-09 21:58:56 +0700

    Trang chủ

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,7 @@
 const userRouter = require("./user.route");
-
+const homeRouter = require("./home.route");
 
 module.exports = (app) => {
-    // app.use("/", userRouter)
+    app.use("/", homeRouter)
     app.use("/users", userRouter)
 } 
