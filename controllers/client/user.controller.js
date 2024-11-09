c0a59b73dc4dd61bd819ff0293682fd18cb80492
hoangphuc1501 <katakurilancer.com>
2024-11-09 21:25:53 +0700

    Đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -36,3 +36,38 @@ module.exports.registerPost = async (req, res) => {
     req.flash("success", "Đăng ký tài khoản thành công!");
     res.redirect("/");
 }
+
+module.exports.login = async (req, res) => {
+
+    res.render("client/pages/users/login.pug",{
+        pageTitle: "Đăng nhập"
+    });
+}
+
+module.exports.loginPost = async (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const existUser = await User.findOne({
+        email: email,
+        deleted: false
+    })
+    if(!existUser){
+        req.flash("error", "Email không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+    if(md5(password) != existUser.password){
+        req.flash("error", "Mật khẩu không chính xác!");
+        res.redirect("back");
+        return;
+    }
+    if(existUser.status != "active"){
+        req.flash("error", "Tài khoản đã bị khóa!")
+        res.redirect("back");
+        return;
+    }
+    res.cookie("tokenUser", existUser.token);
+    req.flash("success", "Đăng nhập thành công!");
+    res.redirect("/");
+}
--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -5,5 +5,6 @@ const controller = require("../../controllers/client/user.controller");
 
 router.get("/register", controller.register);
 router.post("/register", controller.registerPost);
-
+router.get("/login", controller.login);
+router.post("/login", controller.loginPost);
 module.exports = router;
