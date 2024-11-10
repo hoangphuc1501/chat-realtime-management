f04979b92ff3babe30fe43155bc659e21711640b
hoangphuc1501 <katakurilancer.com>
2024-11-10 16:25:00 +0700

    Cài đặt socket

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,5 +1,8 @@
 module.exports.index = async (req, res) => {
-
+    _io.on('connection', (socket) => {
+        console.log('Có 1 user kết nối', socket.id) ;
+    
+    });
     res.render("client/pages/chats/index.pug",{
         pageTitle: "chat"
     });
--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const flash = require('express-flash');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
+const http = require('http');
+const { Server } = require("socket.io");
+
 const app = express();
 const port = 3000;
+
+const server = http.createServer(app);
+const io = new Server(server);
 
 const database = require("./config/database");
 database.connect();
@@ -13,6 +19,9 @@ database.connect();
 app.use(cookieParser('keyboard cat'));
 app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
+
+// khi báo biến toàn cục cho js backend
+global._io = io;
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -28,6 +37,6 @@ app.set('views', `${__dirname}/views`); //Tìm đến thư mục tên là view
 app.set('view engine', 'pug')// template engine sử dụng là pug
 app.use(express.static(`${__dirname}/public`))// thiết lập thư mục chứa file tĩnh
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
