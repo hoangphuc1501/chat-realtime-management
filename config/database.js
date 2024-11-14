450b1ce0230930733ee5a651e320ed14d2bfc144
hoangphuc1501 <katakurilancer.com>
2024-11-14 18:18:20 +0700

    Chỉnh lại cấu hình mongodb

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 module.exports.connect = async () => {
     try {
-        await mongoose.connect(process.env.Mongo_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("kết nối database thành công!")
     } catch (error) {
         console.log("Kết nối database thất bại!")
